0ecb867592f92ee845fdbd7d9e31b2996ef56454
snokeOver <snokeover@gmail.com>
2024-04-28 11:07:14 +0600

    Completed the add spots, get spots based on the user and delete spot based on the id part

--- index.js
+++ index.js
@@ -27,6 +27,53 @@ async function run() {
     if (result.topology.client.topology.s.state === "connected") {
       console.log("MongoDB connection successful!");
     }
+
+    const tourSpotCollection = client.db("tourTangoDB").collection("tour-spot");
+
+    // add tour spots to db
+    app.post("/api/spots/add", async (req, res) => {
+      try {
+        const result = await tourSpotCollection.insertOne(req.body);
+        console.log(result);
+        res.status(201).send({ message: "Tour spot added successfully" });
+      } catch (error) {
+        console.error("Error adding tour spot:", error);
+        res.status(500).send({ message: "Failed to add tour spot" });
+      }
+    });
+
+    // Get all tour spots from db
+    app.get("/api/spots", async (req, res) => {
+      const result = await tourSpotCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Get User's tour spots from db filtered by UID
+    app.get("/api/spots/:uid", async (req, res) => {
+      try {
+        const result = await tourSpotCollection
+          .find({ uid: req.params.uid })
+          .toArray();
+        res.status(200).send(result);
+      } catch (error) {
+        console.error("Error fetching user's tour spots:", error);
+        res.status(500).send({ message: "Internal server error" });
+      }
+    });
+
+    // Delete Single tour spot from db filtered by id
+    app.delete("/api/spot/:id", async (req, res) => {
+      const query = { _id: new ObjectId(req.params.id) };
+      try {
+        const result = await tourSpotCollection.deleteOne(query);
+        res.status(200).send(result);
+      } catch (error) {
+        console.error("Error deleting this tour spot:", error);
+        res.status(500).send({ message: "Internal server error" });
+      }
+    });
+
+    // End of the all API
   } catch (err) {
     console.log(err.message);
   }
@@ -35,9 +82,9 @@ async function run() {
 run().catch(console.error);
 
 app.get("/", (req, res) => {
-  res.send("Coffee server is running!");
+  res.send("TourTango server is running!");
 });
 
 app.listen(serverPort, () => {
-  console.log(`Coffee server is listening on port ${serverPort}`);
+  console.log(`TourTango server is listening on port ${serverPort}`);
 });
